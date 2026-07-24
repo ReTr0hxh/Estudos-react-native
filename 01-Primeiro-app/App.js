dc82edb6461851730b388aee0ddcd7676f792461
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-07-23 22:25:15 -0300

    Código pronto

---
+++ 01-Primeiro-app/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text, Image} from 'react-native';
+
+function App(){
+
+  return(
+    <View>
+      <Text style={{color: 'purple', fontSize: 20, margin: 20}}>Olá mundo!</Text>
+      
+
+      <Logo largura={350} altura={350} nome={'GATOS FOFINHOS'}/>
+
+    </View>
+  );
+}
+
+function Logo(props){
+  let img = 'https://cdn2.thecatapi.com/images/ebv.jpg'
+
+  return(
+    <View>
+      <Image source={{uri: img}} style={{ width: props.largura, height: props.altura}}/>
+      <Text>{props.nome}</Text>
+    </View>
+  );
+
+}
+export default App;
