cc2998b46a350f094837c9eaf89620da8a38b32a
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-07-29 21:47:29 -0300

    aula 3 concluída

---
+++ src/aulas/aula03.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+function Aula02(){
+
+  return(
+    <View style={styles.area}>
+
+      <Text style={[styles.titulo, styles.textoAlinhado]}>oie</Text>
+      <Text style={styles.titulo}>20</Text>
+      <Text style={[styles.subTitulo, styles.textoAlinhado]}>alou</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    area:{
+        marginTop: 20
+    },
+    titulo:{
+        fontSize: 20,
+        color: '#fb00ff'
+    },
+    subTitulo:{
+        color: 'rgba(0, 255, 238, 0.96)'
+    },
+    textoAlinhado:{
+        textAlign: 'center'
+    }
+});
+
+
+
+
+
+
+
+
+
+
+export default Aula02;
