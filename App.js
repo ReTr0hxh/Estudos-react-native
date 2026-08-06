21e11fab5a2a8fd222857931f7b64bf69ed1f34c
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-05 21:38:07 -0300

    ...

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import Aula04 from './src/aulas/aula04';
+import Aula04 from './src/aulas/aula04b_miniProjeto';
 
 export default function App() {
   return (
---
+++ src/aulas/aula4b_minProjeto.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native'
+
+function Aula04b(){
+
+    <view style={{flex:1, backgroundColor: '#121212'}}>
+        <View style={{height: 65, backgroundColor: '#121212'}}></View>
+
+        <View style={{flex:1, backgroundColor: '#ddd'}}></View>
+
+        <View style={{flex: 65, backgroundColor: '#121212'}}></View>
+
+
+    </view>
+
+
+}
+
+const estilos = StyleSheet.create({
+    titulo:{
+        marginTop: 20,
+        color: '#ba21e4',
+        textAlign: 'center',
+        fontSize: 20
+    }
+
+})
+
+export default Aula04b
