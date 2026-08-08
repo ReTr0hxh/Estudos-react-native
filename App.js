556ed5b3c907765f7e40d31db6a827b75bd10a93
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-07 23:53:51 -0300

    Flexbox e alinhamentos

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import Aula04 from './src/aulas/aula04b_miniProjeto';
+import Aula05 from './src/aulas/aula05';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Aula04 />
+      <Aula05 />
     </View>
   );
 }
---
+++ src/aulas/aula05.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+function Aula05(){
+
+  return(
+    <View style={{
+        flex:1, 
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    }}>
+
+        <View style={{ height: 65, width: 50, backgroundColor: '#fffb02'}}></View>
+
+        <View style={{height: 65, width: 50, backgroundColor: '#0ae247'}}></View>
+
+        <View style={{ height: 65, width: 50, backgroundColor: '#bc1bca'}}></View>
+        
+    </View>
+  );
+}
+
+
+export default Aula05;
