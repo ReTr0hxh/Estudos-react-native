70f208acb451d299ad82a18c502ed3be84acf175
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-11 20:17:38 -0300

    Atualizando nomes

--- src/aulas/aula05.js
+++ src/aulas/aula05.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 
-function Aula05(){
+function Aula04(){
 
   return(
-    <View style={{
-        flex:1, 
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-evenly'
-    }}>
+    <View style={{flex:1, backgroundColor: '#121212'}}>
 
-        <View style={{ height: 65, width: 50, backgroundColor: '#fffb02'}}></View>
+        <View style={{ height: 65, backgroundColor: '#121212'}}></View>
 
-        <View style={{height: 65, width: 50, backgroundColor: '#0ae247'}}></View>
+        <View style={{flex:1, backgroundColor: '#DDD'}}></View>
 
-        <View style={{ height: 65, width: 50, backgroundColor: '#bc1bca'}}></View>
+        <View style={{ height: 65, backgroundColor: '#121212'}}></View>
         
     </View>
   );
 }
 
 
-export default Aula05;
+export default Aula04;
