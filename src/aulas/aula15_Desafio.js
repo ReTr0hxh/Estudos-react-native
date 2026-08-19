92726dbec1aef70d9aa82ae73b8660b2e2ed2fbd
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-19 20:39:39 -0300

    Desafio Pomodoro

--- src/aulas/aula15_Desafio.js
+++ src/aulas/aula15_Desafio.js
@@ -1,17 +1,65 @@
 import React, {useState}from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-
+let tempo = null;
 let ss = 0;
-let mm = 25;
+let mm = 1;
+let numeroCiclos = 1;
 
 export default function aula15() {
 
-    const [timer, setTimer] = useState(mm + ":" + 0 + ss);
+    const [timer, setTimer] = useState(mm + ":0" + ss);
+    const [start, setStart] = useState("START")
+    const [ciclo, setCiclo] = useState(1)
+    const [acabou, setAcabou] = useState(false)
 
 
     function Start() {
-        
+
+
+        if (start == "START") {
+            tempo = setInterval(()=>{
+                if (ss == 0) {
+                    ss = 60;
+                    mm--;
+                }
+                if (ss > 0) {
+                    ss--;
+                }
+
+                let fomart = (mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss)
+
+                setTimer(fomart)
+
+                mm == 0 && ss == 0 ? clearInterval(tempo) : null;
+
+                if (mm == 0 && ss == 0) {
+                    numeroCiclos++;
+                    setCiclo(numeroCiclos)
+                    ss = 0;
+                    mm = 25;
+                    setTimer((mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss))
+                    setStart("START");
+                    
+                }
+
+            }, 1)
+            setStart("PAUSE")
+       }else{
+            clearInterval(tempo);
+            setStart("START")
+       }
+
+    }
+
+    function Next() {
+        numeroCiclos++;
+        setCiclo(numeroCiclos)
+        ss = 0;
+        mm = 25;
+        setTimer((mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss))
+        setStart("START");
+        clearInterval(tempo)
     }
 
     
@@ -25,20 +73,26 @@ export default function aula15() {
 
                 <Text style={[styles.textMain, {fontSize: 60}]}>{timer}</Text>
 
-                <TouchableOpacity style={styles.btnPomodoro} onPress={()=>Start()}>
-                    <Text style={[styles.textMain, {fontSize: 30, color: 'red'}]}>START</Text>
-                </TouchableOpacity>
+                <View style={{flexDirection: 'row'}}>
+                    <TouchableOpacity style={styles.btnStart} onPress={()=>Start()}>
+                        <Text style={[styles.textMain, {fontSize: 30, color: 'red'}]}>{start}</Text>
+                    </TouchableOpacity>
 
+                    {start == "PAUSE" ? 
+                    <TouchableOpacity style={[{marginLeft: 10}]} onPress={()=>Next()}>
+                        <Text style={[styles.textMain, styles.btnDesativado, {fontSize: 30, color: 'white'}]}>{"->"}</Text>
+                    </TouchableOpacity> 
+                    : null}
+                </View>
+                
 
 
             </View>
 
 
-            <Text style={[styles.textMain, {marginTop: 40}]}>#1</Text>
+            <Text style={[styles.textMain, {marginTop: 40}]}>#{ciclo}</Text>
 
             <Text style={styles.textMain}>Time to Focus!</Text>
-
-
 
 
         </View>
@@ -65,13 +119,19 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold'
     },
-    btnPomodoro: {
+    btnStart: {
         backgroundColor: 'white',
         width: 120,
         height: 50,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    btnNext: {
+
+    },
+    btnDesativado: {
+        
     }
+    
     
     
 
