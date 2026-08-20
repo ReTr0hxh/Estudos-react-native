981dba6cd0fa9a7412d9b36c2e59d2c8b65e9745
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-20 20:52:57 -0300

    aula 15

--- src/aulas/aula15_Desafio.js
+++ src/aulas/aula15_Desafio.js
@@ -1,5 +1,5 @@
 import React, {useState}from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
 
 let tempo = null;
 let ss = 0;
@@ -11,39 +11,35 @@ export default function aula15() {
     const [timer, setTimer] = useState(mm + ":0" + ss);
     const [start, setStart] = useState("START")
     const [ciclo, setCiclo] = useState(1)
-    const [acabou, setAcabou] = useState(false)
+    const [pausa, setPausa] = useState(false);
+    const [advice, setAdvice] = useState("Time to Focus!")
 
 
     function Start() {
 
-
         if (start == "START") {
             tempo = setInterval(()=>{
-                if (ss == 0) {
-                    ss = 60;
-                    mm--;
-                }
-                if (ss > 0) {
-                    ss--;
-                }
+               if (ss === 0) { ss = 59; mm--; } else { ss--; }
 
                 let fomart = (mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss)
 
                 setTimer(fomart)
 
-                mm == 0 && ss == 0 ? clearInterval(tempo) : null;
+                mm === 0 && ss === 0 ? clearInterval(tempo) : null;
 
-                if (mm == 0 && ss == 0) {
+                if (mm === 0 && ss === 0) {
                     numeroCiclos++;
                     setCiclo(numeroCiclos)
+                    Break();
                     ss = 0;
                     mm = 25;
                     setTimer((mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss))
                     setStart("START");
-                    
+                    Break();
+                    seachAdvice();
                 }
 
-            }, 1)
+            }, 1000)
             setStart("PAUSE")
        }else{
             clearInterval(tempo);
@@ -52,33 +48,69 @@ export default function aula15() {
 
     }
 
+    function Break() {
+        if (pausa === false) {
+            setPausa(true)
+            ss = 0;
+            mm = 5;
+            setTimer((mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss))
+            setStart("START");
+            seachAdvice();
+        }else{
+            setPausa(false)
+        }
+    }
+
     function Next() {
-        numeroCiclos++;
-        setCiclo(numeroCiclos)
+        if (pausa === false) {
+            numeroCiclos++;
+            setCiclo(numeroCiclos)
+        }
         ss = 0;
         mm = 25;
         setTimer((mm < 10 ? "0" + mm : mm) + ":" + (ss == 0 || ss < 10 ? "0" + ss : ss))
         setStart("START");
         clearInterval(tempo)
+        Break();
+        seachAdvice();
     }
 
-    
+    async function seachAdvice() {
+
+        setAdvice("...")
+        if (pausa === false) {
+            try {
+            const r = await fetch(`https://api.adviceslip.com/advice?t=${Math.random()}`);
+            const d = await r.json();
+            setAdvice(d.slip.advice)
+            } catch (erro) {
+                console.log("Erro ao buscar o conselho!")
+            }
+        }else{
+            setAdvice("Time to Focus!")
+        }
+    }
+
+
+
     return(
 
-        <View style={styles.container}>
+        <View style={pausa === true ? styles.containerBreak : styles.container}>
 
-            <Text style={[styles.textMain, {fontSize: 40, marginBottom: 100, marginTop: 10}]}>Pomodoro</Text>
+            <Text style={[styles.textMain, {fontSize: 40, marginBottom: 100, marginTop: 10}]}>FocusFlow</Text>
 
-            <View style={styles.pomodoro}>
+            <View style={pausa === true ? styles.pomodoroBreak : styles.pomodoro}>
 
                 <Text style={[styles.textMain, {fontSize: 60}]}>{timer}</Text>
 
                 <View style={{flexDirection: 'row'}}>
-                    <TouchableOpacity style={styles.btnStart} onPress={()=>Start()}>
-                        <Text style={[styles.textMain, {fontSize: 30, color: 'red'}]}>{start}</Text>
+                    <TouchableOpacity style={styles.btnStart} onPress={()=>Start()} disabled={ciclo > 9}>
+                        <Text 
+                        style={pausa === true ? [styles.textMain, {fontSize: 30, color: '#38858A'}] : [styles.textMain, {fontSize: 30, color: 'red'}]}>{start}
+                        </Text>
                     </TouchableOpacity>
 
-                    {start == "PAUSE" ? 
+                    {start === "PAUSE" ? 
                     <TouchableOpacity style={[{marginLeft: 10}]} onPress={()=>Next()}>
                         <Text style={[styles.textMain, styles.btnDesativado, {fontSize: 30, color: 'white'}]}>{"->"}</Text>
                     </TouchableOpacity> 
@@ -92,7 +124,9 @@ export default function aula15() {
 
             <Text style={[styles.textMain, {marginTop: 40}]}>#{ciclo}</Text>
 
-            <Text style={styles.textMain}>Time to Focus!</Text>
+            <View style={pausa === true ? { width: "auto", height: 'auto', alignItems: 'center'} : styles.container}>
+                <Text style={[styles.textMain, {fontSize: 14}]}>{advice}</Text>
+            </View>
 
 
         </View>
@@ -125,13 +159,18 @@ const styles = StyleSheet.create({
         height: 50,
         alignItems: 'center'
     },
-    btnNext: {
-
+    containerBreak: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#38858A'
     },
-    btnDesativado: {
-        
-    }
-    
+    pomodoroBreak: {
+        backgroundColor: '#4C9196',
+        width: 250,
+        height: 200,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
     
     
 
