61f345241791a489d4305098232b31cd9b2d4c0b
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-25 17:18:05 -0300

    aula 16

--- src/aulas/aula16.js
+++ src/aulas/aula16.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, Text} from 'react-native';
 
 
 export default function Aula14() {
+
+  const [feed, setFeed] = useState([
+      {id: '1', nome: 'a'},
+      {id: '2', nome: 'b'},
+      {id: '3', nome: 'c'},
+      {id: '4', nome: 'd'},
+    ]);
+
+
   return (
     <View style={styles.container}>
+      <FlatList
+        data={feed}
+        renderItem={ ({item}) => <Add data={item} />}
+      />
 
-        
     </View>
   );
 }
@@ -15,4 +27,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  areaPessoa: {
+    backgroundColor: '#951616',
+    height: 200,
+    marginBottom: 15
+  },
+  textoPessoa: {
+    color: '#fff',
+  }
 });
+
+function Add(props) {
+    return(
+      <View style={styles.areaPessoa}>
+        <Text style={styles.textoPessoa}> {props.data.id} </Text>
+        <Text style={styles.textoPessoa}> {props.data.nome} </Text>
+      </View>
+    )
+}
