a05ca56c11c88afad12f3f1e2fbc9eabac953aca
Rafael Freecss <206948776+ReTr0hxh@users.noreply.github.com>
2026-08-31 22:20:06 -0300

    aula 21

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import Aula from './src/aulas/aulas_11_a_21/aula_20/aula20';
+import Aula from './src/aulas/aulas_11_a_50/aula_21/aula21';
 
 export default function App() {
 
---
+++ src/aulas/aulas_11_a_50/aula_21/aula21.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import { View, StyleSheet, Text, Switch } from 'react-native';
+
+export default function App() {
+    const [status, setStatus] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      
+       <Switch
+        value={status}
+        onValueChange={(valorSelecionado)=>setStatus(valorSelecionado)}
+       />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
